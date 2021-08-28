617f6d0d58e069917d1b7480a146987240681d95
UserBlackBox <userblackbox@tutanota.com>
2021-08-28 01:55:45 -0400

    add max lengths to charfields

--- cfTracker/models/purchase.py
+++ cfTracker/models/purchase.py
@@ -8,13 +8,14 @@ class Purchase(models.Model):
         default=lambda: secrets.token_urlsafe(10),
         unique=True,
         editable=False,
-        blank=False
+        blank=False,
+        max_length=10
     )
-    store = models.CharField()
+    store = models.CharField(max_length=80)
     time = models.DateTimeField(auto_now=True)
 
 
 class Item(models.Model):
     purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE, related_name='items', blank=False)
-    item = models.CharField(blank=False)
+    item = models.CharField(blank=False, max_length=80)
     quantity = models.FloatField()
